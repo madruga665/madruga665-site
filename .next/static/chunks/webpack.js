468f86f3496a4f9c6419ba44d4b7065971b53386
Luciano Amâncio <madruga665@hotmail.com>
2023-02-25 14:09:05 -0300

    remove index antigo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3b25a2a45dab687"; }
+/******/ 		__webpack_require__.h = function() { return "1ace320a5092faae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
