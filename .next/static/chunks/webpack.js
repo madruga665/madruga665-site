3c1eb1d63f21333f0419724cf5b61e4fd575066e
Luciano Amâncio <madruga665@hotmail.com>
2023-02-25 14:05:55 -0300

    remove o app antigo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "efdb00daa406e655"; }
+/******/ 		__webpack_require__.h = function() { return "a3b25a2a45dab687"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
