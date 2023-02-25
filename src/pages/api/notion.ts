28f910c095489b474f63fff2e32549f1e815ec16
Luciano Amâncio <madruga665@hotmail.com>
2023-02-25 18:27:43 -0300

    refactor(notionApi): add try catch na função getHomePageData

--- src/pages/api/notion.ts
+++ src/pages/api/notion.ts
@@ -2,17 +2,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const { Client } = require("@notionhq/client");
 
-// Initializing a client
-const notion = new Client({
+const notionClient = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-const notionService = async (_req: NextApiRequest, res: NextApiResponse) => {
-  const homePage = await notion.pages.retrieve({
-    database_id: process.env.NOTION_DATABASE_ID,
-    page_id: 'e3d05fb599c24fa98a346d67b3c9f956'
-  });
-  res.status(200).json(homePage.properties.presentation.rich_text[0].text.content);
+const getHomePageData = async (_req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const homePage = await notionClient.pages.retrieve({
+      database_id: process.env.NOTION_DATABASE_ID,
+      page_id: "e3d05fb599c24fa98a346d67b3c9f956",
+    });
+    res.status(200).json(homePage.properties.presentation.rich_text[0].text.content);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export default notionService;
+export default getHomePageData;
