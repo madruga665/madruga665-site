e4a316619f9af0775657617266659e274f569c0d
Luciano Amâncio <madruga665@hotmail.com>
2023-02-25 16:19:38 -0300

    remove .next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/portifolio":["static\u002Fchunks\u002Fpages\u002Fportifolio.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fportifolio"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
