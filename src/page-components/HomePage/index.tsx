2d037201f0f10af458278c97c9b9eecb5dae1cbf
Luciano Amâncio <madruga665@hotmail.com>
2023-02-28 20:26:41 -0300

    feat(HomePage): add component image do next js

--- src/page-components/HomePage/index.tsx
+++ src/page-components/HomePage/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+
 import { HomePageProps } from "../../interfaces/homePage";
 import PageTemplate from "../../templates/PageTemplate";
 import styles from "./styles.module.scss";
@@ -11,7 +13,13 @@ function HomePage({ image, presentation }: HomePageProps) {
       </Head>
       <section className={styles.Container}>
         <div className={styles.ImageContainer}>
-          <img className={styles["about-image"]} src={image} alt='Luciano Amancio' />
+          <Image
+            width={350}
+            height={350}
+            className={styles["About-image"]}
+            src={image}
+            alt='Luciano Amancio'
+          />
         </div>
         <div className={styles.TextContainer}>
           <p>{presentation}</p>
