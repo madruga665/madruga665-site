22a5374f79f6ed6480ae2a395768175d027768a0
Luciano Amâncio <madruga665@hotmail.com>
2023-03-09 08:49:59 -0300

    feat: add Head com titulo para as paginas q estavam sem

--- src/page-components/AboutPage/index.tsx
+++ src/page-components/AboutPage/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import NewsSoon from "../../components/NewsSoon";
 import PageTemplate from "../../templates/PageTemplate";
 
@@ -5,6 +6,9 @@ function AboutPage() {
   const title = "Logo mais vai ter coisa aqui!";
   return (
     <PageTemplate>
+      <Head>
+        <title>Luciano Amâncio - Sobre</title>
+      </Head>
       <NewsSoon title={title} />
     </PageTemplate>
   );
--- src/page-components/ProjectsPage/index.tsx
+++ src/page-components/ProjectsPage/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import NewsSoon from "../../components/NewsSoon";
 import PageTemplate from "../../templates/PageTemplate";
 
@@ -5,6 +6,9 @@ function ProjectsPage() {
   const title = "Logo mais vai ter coisa aqui!";
   return (
     <PageTemplate>
+      <Head>
+        <title>Luciano Amâncio - Projetos</title>
+      </Head>
       <NewsSoon title={title} />
     </PageTemplate>
   );
