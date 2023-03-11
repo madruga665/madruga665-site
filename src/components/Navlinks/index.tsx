4293432a719c8369b33282f97a47d028316b8f14
Luciano Amâncio <madruga665@hotmail.com>
2023-03-11 09:50:08 -0300

    chore(NavLinks): add propriedade key

--- src/components/Navlinks/index.tsx
+++ src/components/Navlinks/index.tsx
@@ -1,33 +1,29 @@
 "use client";
 import Link from "next/link";
 
-import EnumHelper from "../../helpers/enumHelper";
-import useHighlightPath from "../../hooks/usePath";
+import EnumHelper from "@/helpers/enumHelper";
+import useHighlightPath from "@/hooks/usePath";
 
 import styles from "./styles.module.scss";
-
-interface ILink {
-  title: string;
-  link: string;
-}
 
 function NavLinks() {
   const enumHelper = new EnumHelper();
   const { isCurrentPage } = useHighlightPath();
 
-  function buildLinks(link: ILink, index: number) {
-    return (
-      <div className={isCurrentPage(link.link) ? styles.LinkContainerActive : styles.LinkContainer}>
-        <li className={styles.StyledListItem} key={index}>
-          <Link href={link.link}>{link.title}</Link>
-        </li>
-      </div>
-    );
-  }
-
   return (
     <nav className={styles.Links}>
-      <ul>{enumHelper.links.map(buildLinks)}</ul>
+      <ul>
+        {enumHelper.links.map((link) => (
+          <div
+            className={isCurrentPage(link.link) ? styles.LinkContainerActive : styles.LinkContainer}
+            key={link.title}
+          >
+            <li className={styles.StyledListItem}>
+              <Link href={link.link}>{link.title}</Link>
+            </li>
+          </div>
+        ))}
+      </ul>
     </nav>
   );
 }
