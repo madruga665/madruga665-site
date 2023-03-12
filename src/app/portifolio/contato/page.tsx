8ab3ebb2c7987f30f8263095282c50f21bb1b44f
Luciano Amâncio <madruga665@hotmail.com>
2023-03-12 11:16:49 -0300

    chore: add pagina de contado no diretorio app

---
+++ src/app/portifolio/contato/page.tsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+
+import Button from "@/components/Button";
+
+import styles from "./styles.module.scss";
+
+interface formData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+export const metadata = {
+  title: "Luciano Amâncio - Contato",
+};
+
+function ContactPage() {
+  const { register, handleSubmit, formState, reset } = useForm<formData>({
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+  const { errors, isSubmitting } = formState;
+
+  async function onSubmit(data: formData) {
+    reset();
+    alert("mensagem enviada com sucesso");
+    await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/contact`, data);
+  }
+
+  return (
+    <div className={styles.Container}>
+      <form className={styles.Form} onSubmit={handleSubmit(onSubmit)}>
+        <p className={styles.Text}>
+          Caso queira entrar em contado ou deixar algum feedback fique à vontade para me mandar uma
+          mensagem.
+        </p>
+        <div className={styles.FormControl}>
+          <label htmlFor='name'>Seu Nome</label>
+          <input id='name' type='text' {...register("name", { required: true })} />
+          {errors.name && <span className={styles.ErrorText}>Nome obrigatório</span>}
+        </div>
+
+        <div className={styles.FormControl}>
+          <label htmlFor='email'>Seu Email</label>
+          <input id='email' type='email' {...register("email", { required: true })} />
+          {errors.email && <span className={styles.ErrorText}>Email obrigatório</span>}
+        </div>
+
+        <div className={styles.FormControl}>
+          <label htmlFor='message'>Sua Mensagem</label>
+          <textarea id='message' {...register("message", { required: true })} />
+          {errors.message && <span className={styles.ErrorText}>Mensagem obrigatória</span>}
+        </div>
+        <div className={styles.ButtonContainer}>
+          <Button type='submit' label='Enviar' loading={isSubmitting} />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ContactPage;
--- src/pages/portifolio/contato.tsx
+++
@@ -1,7 +0,0 @@
-import ContactPage from "../../page-components/ContactPage";
-
-function Contact() {
-  return <ContactPage />;
-}
-
-export default Contact;
