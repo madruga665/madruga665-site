2860863163c17c51337b95a7d8735fd445b7efa1
Luciano Amâncio <madruga665@hotmail.com>
2023-03-19 17:51:14 -0300

    feat: add classe css com animação de carregamento da pagina

--- src/app/portifolio/contato/page.tsx
+++ src/app/portifolio/contato/page.tsx
@@ -10,6 +10,7 @@ import Button from "@/components/Button";
 import "react-toastify/dist/ReactToastify.minimal.css";
 import styles from "./styles.module.scss";
 import EnumHelper from "@/helpers/enumHelper";
+import WindowCard from "@/components/WindowCard";
 
 interface formData {
   name: string;
@@ -48,40 +49,42 @@ function ContactPage() {
       <Head>
         <title>Luciano Amâncio - Contato</title>
       </Head>
-      <form
-        className={styles.Form}
-        onSubmit={handleSubmit((data) =>
-          onSubmit(data, () => {
-            reset();
-            router.refresh();
-          })
-        )}
-      >
-        <p className={styles.Text}>
-          Caso queira entrar em contado ou deixar algum feedback fique à vontade para me mandar uma
-          mensagem.
-        </p>
-        <div className={styles.FormControl}>
-          <label htmlFor='name'>Seu Nome</label>
-          <input id='name' type='text' {...register("name", { required: true })} />
-          {errors.name && <span className={styles.ErrorText}>Nome obrigatório</span>}
-        </div>
+      <WindowCard>
+        <form
+          className={styles.Form}
+          onSubmit={handleSubmit((data) =>
+            onSubmit(data, () => {
+              reset();
+              router.refresh();
+            })
+          )}
+        >
+          <p className={styles.Text}>
+            Caso queira entrar em contado ou deixar algum feedback fique à vontade para me mandar
+            uma mensagem.
+          </p>
+          <div className={styles.FormControl}>
+            <label htmlFor='name'>Seu Nome</label>
+            <input id='name' type='text' {...register("name", { required: true })} />
+            {errors.name && <span className={styles.ErrorText}>Nome obrigatório</span>}
+          </div>
 
-        <div className={styles.FormControl}>
-          <label htmlFor='email'>Seu Email</label>
-          <input id='email' type='email' {...register("email", { required: true })} />
-          {errors.email && <span className={styles.ErrorText}>Email obrigatório</span>}
-        </div>
+          <div className={styles.FormControl}>
+            <label htmlFor='email'>Seu Email</label>
+            <input id='email' type='email' {...register("email", { required: true })} />
+            {errors.email && <span className={styles.ErrorText}>Email obrigatório</span>}
+          </div>
 
-        <div className={styles.FormControl}>
-          <label htmlFor='message'>Sua Mensagem</label>
-          <textarea id='message' {...register("message", { required: true })} />
-          {errors.message && <span className={styles.ErrorText}>Mensagem obrigatória</span>}
-        </div>
-        <div className={styles.ButtonContainer}>
-          <Button type='submit' label='Enviar' loading={isSubmitting} />
-        </div>
-      </form>
+          <div className={styles.FormControl}>
+            <label htmlFor='message'>Sua Mensagem</label>
+            <textarea id='message' {...register("message", { required: true })} />
+            {errors.message && <span className={styles.ErrorText}>Mensagem obrigatória</span>}
+          </div>
+          <div className={styles.ButtonContainer}>
+            <Button type='submit' label='Enviar' loading={isSubmitting} />
+          </div>
+        </form>
+      </WindowCard>
     </div>
   );
 }
