b8fc16a2a6122a991c3e31d8652d8802e1887d74
Luciano Amâncio <madruga665@hotmail.com>
2023-03-25 11:43:34 -0300

    feat(projetos): add service fetch sem cache dos dados

--- src/app/portifolio/projetos/page.tsx
+++ src/app/portifolio/projetos/page.tsx
@@ -1,22 +1,14 @@
-import axios from "axios";
-
 import { ProjectSerealized } from "@/interfaces/projectsPage";
 import ProjectCard from "@/components/ProjectCard";
 
 import styles from "./styles.module.scss";
 import WindowCard from "@/components/WindowCard";
+import { getProjectsPageData } from "./projects.service";
 
 export const metadata = {
   title: "Luciano Amâncio - Projetos",
   description: "Projetos desenvolvidos por Luciano Amâncio",
 };
-
-async function getProjectsPageData(): Promise<ProjectSerealized[]> {
-  const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/projects`);
-  const data = await response.data;
-
-  return data;
-}
 
 async function Projects() {
   const projects = await getProjectsPageData();
---
+++ src/app/portifolio/projetos/projects.service.ts
@@ -0,0 +1,14 @@
+import { ProjectSerealized } from "@/interfaces/projectsPage";
+
+export async function getProjectsPageData(): Promise<ProjectSerealized[]> {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/projects`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  });
+  const data = await response.json();
+
+  return data;
+}
