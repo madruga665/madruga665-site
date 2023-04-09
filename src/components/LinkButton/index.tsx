c50b5e73f62f614adb55a3724c3e35765d1db355
Luciano Amâncio <madruga665@hotmail.com>
2023-04-08 23:50:49 -0300

    feat(home): add botão de download do curriculo

--- src/components/LinkButton/index.tsx
+++ src/components/LinkButton/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 
 function LinkButton({ url, children }: { url: string; children: React.ReactNode }) {
   return (
-    <Link className={styles.LinkButton} href={url} target='_blank' rel='noreferrer' >
+    <Link className={styles.linkButton} href={url} target='_blank' rel='noreferrer' >
       {children}
     </Link>
   );
