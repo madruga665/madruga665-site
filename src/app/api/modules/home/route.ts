f95d0d6ec4da17dbc4f93cf58d3022b332d1fd97
Luciano Amâncio <madruga665@hotmail.com>
2023-10-08 01:23:31 -0300

    teste revalidate path

--- src/app/api/modules/home/route.ts
+++ src/app/api/modules/home/route.ts
@@ -1,12 +1,16 @@
-import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
 import NotionAdpter from "../../core/database/notion-adapter";
 import HomeControllerImpl from "./adapters/controller/home-controller-impl";
 import HomeRepositoryImpl from "./adapters/repositories/home-repository-impl";
 import GetHomePageDataUsecase from "./application/usecases/get-home-page-data";
 
-export const fetchCache = "force-no-store";
+// export const fetchCache = "force-no-store";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get("path") || "";
+  revalidatePath(path);
+
   const databaseId = process.env.NOTION_DATABASE_ID || "";
   const homePageId = process.env.NOTION_HOME_PAGE_ID || "";
   const notionToken = process.env.NOTION_TOKEN || "";
